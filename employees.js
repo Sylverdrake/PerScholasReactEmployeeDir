799366a14c9da68423ad862043c632e705897964
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-07-30 21:45:47 -0400

    models

---
+++ employees.js
@@ -0,0 +1,61 @@
+let employee1 =
+{
+    employeeName: "John Teller",
+    employeeTitle: "Company President",
+    officePhone: "123-456-7890",
+    mobilePhone: "123-789-3456",
+    smsPhone: "123-893-9180",
+    employeeEmail: "jteller@bigcompco.com"
+}
+
+let employee2 =
+{
+    employeeName: "Jane Doe",
+    employeeTitle: "Company VP of Marketing",
+    officePhone: "363-436-7311",
+    mobilePhone: "833-792-3746",
+    smsPhone: "113-913-9580",
+    employeeEmail: "jDoe@bigcompco.com"
+}
+
+let employee3 =
+{
+    employeeName: "Daniel Doe",
+    employeeTitle: "CFO of Nepotism",
+    officePhone: "113-794-1236",
+    mobilePhone: "833-789-3326",
+    smsPhone: "113-113-9580",
+    employeeEmail: "dDoe@bigcompco.com"
+}
+
+let employee4 =
+{
+    employeeName: "Nathaniel Nathan",
+    employeeTitle: "Company VP of Engineering",
+    officePhone: "212-963-8822",
+    mobilePhone: "823-532-3006",
+    smsPhone: "164-663-9120",
+    employeeEmail: "nNathan@bigcompco.com"
+}
+
+let employee5 =
+{
+    employeeName: "Guy Wyse",
+    employeeTitle: "Company VP of Sales",
+    officePhone: "963-451-8522",
+    mobilePhone: "523-492-5246",
+    smsPhone: "100-993-7260",
+    employeeEmail: "gWyse@bigcompco.com"
+}
+
+let employee6 =
+{
+    employeeName: "Ann Pun",
+    employeeTitle: "QA Manager",
+    officePhone: "123-123-1231",
+    mobilePhone: "123-735-3346",
+    smsPhone: "323-123-1234",
+    employeeEmail: "aPun@bigcompco.com"
+}
+
+module.exports = [employee1, employee2, employee3, employee4, employee5, employee6]
